123fb726ff4db1213f073a87bfad8c0e342e18cc
zenground0 <ZenGround0@users.noreply.github.com>
2021-12-08 12:11:19 -0500

    Rough integration test

--- itests/ccupgrade_test.go
+++ itests/ccupgrade_test.go
@@ -10,10 +10,10 @@ import (
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/itests/kit"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-// TODO: This needs to be repurposed into a SnapDeals test suite
 func TestCCUpgrade(t *testing.T) {
 	kit.QuietMiningLogs()
 
@@ -30,6 +30,7 @@ func TestCCUpgrade(t *testing.T) {
 }
 
 func runTestCCUpgrade(t *testing.T, upgradeHeight abi.ChainEpoch) {
+	t.Skip()
 	ctx := context.Background()
 	blockTime := 5 * time.Millisecond
 
@@ -41,18 +42,17 @@ func runTestCCUpgrade(t *testing.T, upgradeHeight abi.ChainEpoch) {
 		t.Fatal(err)
 	}
 
-	CC := abi.SectorNumber(kit.DefaultPresealsPerBootstrapMiner + 1)
-	Upgraded := CC + 1
+	CCUpgrade := abi.SectorNumber(kit.DefaultPresealsPerBootstrapMiner + 1)
 
 	miner.PledgeSectors(ctx, 1, 0, nil)
 
 	sl, err := miner.SectorsList(ctx)
 	require.NoError(t, err)
 	require.Len(t, sl, 1, "expected 1 sector")
-	require.Equal(t, CC, sl[0], "unexpected sector number")
+	require.Equal(t, CCUpgrade, sl[0], "unexpected sector number")
 
 	{
-		si, err := client.StateSectorGetInfo(ctx, maddr, CC, types.EmptyTSK)
+		si, err := client.StateSectorGetInfo(ctx, maddr, CCUpgrade, types.EmptyTSK)
 		require.NoError(t, err)
 		require.Less(t, 50000, int(si.Expiration))
 	}
@@ -68,37 +68,6 @@ func runTestCCUpgrade(t *testing.T, upgradeHeight abi.ChainEpoch) {
 	outPath := dh.PerformRetrieval(context.Background(), deal, res.Root, false)
 	kit.AssertFilesEqual(t, inPath, outPath)
 
-	// Validate upgrade
-
-	{
-		exp, err := client.StateSectorExpiration(ctx, maddr, CC, types.EmptyTSK)
-		if err != nil {
-			require.Contains(t, err.Error(), "failed to find sector 3") // already cleaned up
-		} else {
-			require.NoError(t, err)
-			require.NotNil(t, exp)
-			require.Greater(t, 50000, int(exp.OnTime))
-		}
-	}
-	{
-		exp, err := client.StateSectorExpiration(ctx, maddr, Upgraded, types.EmptyTSK)
-		require.NoError(t, err)
-		require.Less(t, 50000, int(exp.OnTime))
-	}
-
-	dlInfo, err := client.StateMinerProvingDeadline(ctx, maddr, types.EmptyTSK)
-	require.NoError(t, err)
-
-	// Sector should expire.
-	for {
-		// Wait for the sector to expire.
-		status, err := miner.SectorsStatus(ctx, CC, true)
-		require.NoError(t, err)
-		if status.OnTime == 0 && status.Early == 0 {
-			break
-		}
-		t.Log("waiting for sector to expire")
-		// wait one deadline per loop.
-		time.Sleep(time.Duration(dlInfo.WPoStChallengeWindow) * blockTime)
-	}
+	status, err := miner.SectorsStatus(ctx, CCUpgrade, true)
+	assert.Equal(t, 1, len(status.Deals))
 }
